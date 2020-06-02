4d013432aa219f8d63680409238d512084b76f84
Jethro <jethrobar@gmail.com>
2020-06-02 19:31:48 +0200

    Add project files.

---
+++ QuickHash.ConsoleNet/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickHash.Gen;
+
+namespace QuickHash.ConsoleNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader();
+
+            string psw = @"Q@zw1234";
+            bool ImIn = false;
+            while (!ImIn)
+            {
+                Console.Write("Password: ");
+                Console.CursorVisible = false;
+
+                if (psw.Equals(PromptPass()))
+                    ImIn = true;
+                else
+                    Console.WriteLine("\nInvalid password. Please try again...\n");
+
+            }
+
+            Console.Clear();
+            PrintHeader();
+            Console.CursorVisible = true;
+
+            HashGenerator gen;
+
+            while (true)
+            {
+                Console.WriteLine("Insert a string to generate hash or ctrl+c to quit:");
+                string input = Console.ReadLine();
+
+                gen = new HashGenerator(input);
+
+                Console.WriteLine($"\nOutput: {gen.HashIt()}\n");
+                Console.Write("Export token? (y/n): ");
+
+                if (Console.ReadLine() == "y")
+                {
+                    gen.ExportTokenFile();
+                    Console.WriteLine("\nFile exported to desktop...");
+                }
+
+                Console.WriteLine("\nPress any key to restart or ctrl+c to quit...");
+                Console.ReadLine();
+                Console.Clear();
+                PrintHeader();
+            }
+        }
+
+        private static string PromptPass()
+        {
+            bool contReading = true;
+            StringBuilder sb = new StringBuilder();
+
+            char newLineChar = '\r';
+
+            while (contReading)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+                char pswChar = consoleKeyInfo.KeyChar;
+
+                if (pswChar == newLineChar)
+                    contReading = false;
+                else
+                    sb.Append(pswChar.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        private static void PrintHeader()
+        {
+            string header = @"
+ _____            _____            
+/  __ \          |  __ \           
+| /  \/_ __ _   _| |  \/ ___ _ __  
+| |   | '__| | | | | __ / _ \ '_ \ 
+| \__/\ |  | |_| | |_\ \  __/ | | |
+ \____/_|   \__, |\____/\___|_| |_|
+             __/ |                 
+            |___/                  " + Environment.NewLine;
+
+            Console.WriteLine(header);
+
+        }
+    }
+}
---
+++ QuickHash.Gen/HashGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace QuickHash.Gen
+{
+    public class HashGenerator
+    {
+        private readonly string _input;
+        private string _output;
+
+        public HashGenerator(string input)
+        {
+            _input = input;
+        }
+
+        public HashGenerator() : this(string.Empty) { }
+
+        public string HashIt()
+        {
+            StringBuilder sb = new StringBuilder();
+            var fullInput = _input + DateTime.Now.ToString("yyyyMMdd");
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(fullInput));
+
+                foreach (byte b in result)
+                    sb.Append(b.ToString("X2"));
+            }
+
+            _output = sb.ToString();
+
+            return _output;
+        }
+
+        public void ExportTokenFile()
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\token.txt";
+
+            if (!string.IsNullOrEmpty(_output))
+                File.WriteAllText(filePath, _output);
+        }
+    }
+}
